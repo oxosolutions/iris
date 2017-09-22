bf1ad5765576e6f2bba1f712697a7530a8dbf272
Rahul <rahulsharma841990@outlook.com>
2017-09-22 16:23:39 +0530

    update new design

--- www/js/dashboard.js
+++ www/js/dashboard.js
@@ -86,9 +86,9 @@ angular.module('smaart.dashboard', ['ngCordova'])
 	    $scope.modal.hide();
 	  };
 	  // Cleanup the modal when we're done with it!
-	  $scope.$on('$destroy', function() {
+	  /*$scope.$on('$destroy', function() {
 	    $scope.modal.remove();
-	  });
+	  });*/
 	  // Execute action on hide modal
 	  $scope.$on('modal.hidden', function() {
 	    // Execute action
